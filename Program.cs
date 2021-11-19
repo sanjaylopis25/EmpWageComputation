4a58dc20a036098a027c730247b754e5d2a2796c
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-19 23:16:25 +0530

    modified for the UC5 program

--- Program.cs
+++ Program.cs
@@ -9,34 +9,40 @@ namespace EmpWageComputation
             int Wageperhr = 20;
             const int Fulltime = 1;
             const int Parttime = 2;
+            const int Workingdays = 20;
+            int TotalEmpWage = 0;
             int timehr = 0;
             int EmployeeWage = 0;
-            Random random = new Random();
-            int attendance = random.Next(0, 3);
-            switch (attendance)
+            for (int day=1; day<= Workingdays; day++)
             {
-                case Fulltime :
-                    timehr = 8;
-                    Console.WriteLine("Full time Employee Is Present");
-                    EmployeeWage = Wageperhr * timehr;
-                    Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
-                    Console.ReadLine();
-                    break;
-                case Parttime:
-                    timehr = 4;
-                    Console.WriteLine("Part time Employee Is Present");
-                    EmployeeWage = Wageperhr * timehr;
-                    Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
-                    Console.ReadLine();
-                    break;
-                default:
-                    Console.WriteLine("Employee Is Absent");
-                    timehr = 0;
-                    EmployeeWage = Wageperhr * timehr;
-                    Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
-                    Console.ReadLine();
-                    break;
+                Random random = new Random();
+                int attendance = random.Next(0, 3);
+                switch (attendance)
+                {
+                    case Fulltime:
+                        timehr = 8;
+                        Console.WriteLine("Full time Employee Is Present");
+                        EmployeeWage = Wageperhr * timehr;
+                        TotalEmpWage = TotalEmpWage + EmployeeWage;
+                        Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
+                        break;
+                    case Parttime:
+                        timehr = 4;
+                        Console.WriteLine("Part time Employee Is Present");
+                        EmployeeWage = Wageperhr * timehr;
+                        TotalEmpWage = TotalEmpWage + EmployeeWage;
+                        Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
+                        break;
+                    default:
+                        Console.WriteLine("Employee Is Absent");
+                        timehr = 0;
+                        EmployeeWage = Wageperhr * timehr;
+                        TotalEmpWage = TotalEmpWage + EmployeeWage;
+                        Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
+                        break;
+                }
             }
+            Console.WriteLine("Total Wage of an Employee per 20 days a month is " + TotalEmpWage);
         }
     }
 }
