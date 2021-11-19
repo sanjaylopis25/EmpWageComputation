266a84372386860e9c60311df5d783ca634e312f
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-19 22:52:44 +0530

    modified for the UC4 program

--- Program.cs
+++ Program.cs
@@ -7,35 +7,35 @@ namespace EmpWageComputation
         static void Main(string[] args)
         {
             int Wageperhr = 20;
-            int Fulltime = 1;
-            int Parttime = 2;
+            const int Fulltime = 1;
+            const int Parttime = 2;
             int timehr = 0;
             int EmployeeWage = 0;
             Random random = new Random();
             int attendance = random.Next(0, 3);
-            if (attendance == Fulltime)
+            switch (attendance)
             {
-                timehr = 8;
-                Console.WriteLine("Full time Employee Is Present");
-                EmployeeWage = Wageperhr * timehr;
-                Console.WriteLine("Daily Wage of an Employee is "+EmployeeWage);
-                Console.ReadLine();
-            }
-            else if (attendance == Parttime)
-            {
-                timehr = 4;
-                Console.WriteLine("Part time Employee Is Present");
-                EmployeeWage = Wageperhr * timehr;
-                Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Employee Is Absent");
-                timehr = 0;
-                EmployeeWage = Wageperhr * timehr;
-                Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
-                Console.ReadLine();
+                case Fulltime :
+                    timehr = 8;
+                    Console.WriteLine("Full time Employee Is Present");
+                    EmployeeWage = Wageperhr * timehr;
+                    Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
+                    Console.ReadLine();
+                    break;
+                case Parttime:
+                    timehr = 4;
+                    Console.WriteLine("Part time Employee Is Present");
+                    EmployeeWage = Wageperhr * timehr;
+                    Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Employee Is Absent");
+                    timehr = 0;
+                    EmployeeWage = Wageperhr * timehr;
+                    Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
+                    Console.ReadLine();
+                    break;
             }
         }
     }
