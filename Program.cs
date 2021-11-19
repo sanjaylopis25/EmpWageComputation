905cafee4eaec48e814828de8293f4bb7f2da893
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-19 22:14:38 +0530

    modified for the UC2 program

--- Program.cs
+++ Program.cs
@@ -6,16 +6,25 @@ namespace EmpWageComputation
     {
         static void Main(string[] args)
         {
+            int Wageperhr = 20;
+            int Fulltime = 0;
+            int EmployeeWage = 0;
             Random random = new Random();
             int attendance = random.Next(0, 2);
             if (attendance == 1)
             {
+                Fulltime = 8;
                 Console.WriteLine("Employee Is Present");
+                EmployeeWage = Wageperhr * Fulltime;
+                Console.WriteLine("Daily Wage of an Employee is "+EmployeeWage);
                 Console.ReadLine();
             }
             else
             {
                 Console.WriteLine("Employee Is Absent");
+                Fulltime = 0;
+                EmployeeWage = Wageperhr * Fulltime;
+                Console.WriteLine("Daily Wage of an Employee is " + EmployeeWage);
                 Console.ReadLine();
             }
         }
