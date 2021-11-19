b62c942c7e5b525d37d8ba9354f9d7269456dce8
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-19 21:40:41 +0530

    Modified for the UC1 program

--- Program.cs
+++ Program.cs
@@ -6,8 +6,18 @@ namespace EmpWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
-            Console.ReadLine();
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee Is Present");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+                Console.ReadLine();
+            }
         }
     }
 }
