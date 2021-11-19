e3d80a5fbe688daa0dfa30cf0185e615a339a81e
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-19 21:08:33 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            Console.ReadLine();
+        }
+    }
+}
